7bd9f0ba33e950357518611d4af7410cadd2f674
SHIMANTA DAS <shimanta.das@weavers-web.com>
2025-02-07 18:43:11 +0530

    uploaded many images

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ use Faker\Factory;
 $faker = Factory::create();
 
 // Number of blog posts to generate
-$numberOfPosts = 3;
+$numberOfPosts = 10;
 
 // Array to store blog posts
 $blogPosts = [];
@@ -44,13 +44,26 @@ function generateRealisticContent($faker) {
     return $content;
 }
 
+
+$set_image = array(
+  'nature' => 'dummy-images/nature/',
+  'tech' => 'dummy-images/tech/',
+  'travel' => 'dummy-images/travel/',
+  'health' => 'dummy-images/health/',
+  'food' => 'dummy-images/food/',
+  'business' => 'dummy-images/business/',
+  '18+' => 'dummy-images/18+/',
+);
+
 // Generate blog posts
 for ($i = 0; $i < $numberOfPosts; $i++) {
+    $names = ['001.jpg','002.jpg','003.jpg','004.jpg','005.jpg','006.jpg','007.jpg','008.jpg','009.jpg','010.jpg'];
+    $key = array_rand($names);
     $blogPost = [
         'title' => $faker->sentence(6),
         'content' => generateRealisticContent($faker),
         'excerpt' => $faker->paragraph(2) . ' ' . $faker->sentence(),
-        'featured_image' => $faker->imageUrl(800, 400, 'nature', true),
+        'featured_image' => $set_image['travel'].$names[$key],
         'category' => $faker->randomElement(['Technology', 'Travel', 'Food', 'Health', 'Lifestyle']),
         'author' => $faker->name,
         'date' => $faker->date('F j, Y'),
@@ -63,7 +76,7 @@ for ($i = 0; $i < $numberOfPosts; $i++) {
 foreach ($blogPosts as $post) {
     echo "<article style='margin-bottom: 40px; border-bottom: 1px solid #ccc; padding-bottom: 20px;'>";
     echo "Blog Title: <h2 style='color: blue;'>{$post['title']}</h2>";
-    echo "<img src='{$post['featured_image']}' alt='Featured Image' style='width: 100%; max-width: 800px; height: auto; margin-bottom: 20px;'>";
+    echo "<img src='{$post['featured_image']}' alt='Featured Image' style='width: 100%; max-width: 200px; height: 200px; margin-bottom: 20px;'>";
     echo "<p><strong>Category:</strong> {$post['category']}</p>";
     echo "<p><strong>Author:</strong> {$post['author']}</p>";
     echo "<p><strong>Date:</strong> {$post['date']}</p>";
